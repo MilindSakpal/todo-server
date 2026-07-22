dc71f6736dfa40009bffc8097456f38756818976
MilindSakpal <milind52sakpal@gmail.com>
2026-07-22 20:07:50 +0530

    Test4

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -4,6 +4,21 @@ const router = express.Router();
 const { getGraphClient } = require("../services/graphService");
 
 // Get all columns
+router.get("/column-names", async (req, res) => {
+  try {
+    const client = await getGraphClient();
+
+    const columns = await client
+      .api("/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ3DXGXPOPIDDRHZTJA4BFHA2YFH/workbook/tables/TaskTable/columns")
+      .get();
+
+    res.json(columns.value.map(c => c.name));
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/columns", async (req, res) => {
   try {
     const client = await getGraphClient();
@@ -32,25 +47,25 @@ router.post("/add-row", async (req, res) => {
     const client = await getGraphClient();
 
     const response = await client
-      .api(
-        "/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ3DXGXPOPIDDRHZTJA4BFHA2YFH/workbook/tables/TaskTable/rows/add"
-      )
-      .post({
-        values: [[
-          1,
-          "2026-07-22",
-          "Office Time Sheet",
-          "Testing Graph API",
-          "Development",
-          "High",
-          2,
-          2,
-          "Completed",
-          "Office",
-          "",
-          "Successfully added from Graph API"
-        ]]
-      });
+  .api(
+    "/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ3DXGXPOPIDDRHZTJA4BFHA2YFH/workbook/tables/TaskTable/rows/add"
+  )
+  .post({
+    values: [[
+      "",
+      "2026-07-22",
+      "Internal",
+      "Testing Graph API",
+      "Medium",
+      "High",
+      2,
+      2,
+      "Done",
+      "Office",
+      "",
+      "Successfully added from Graph API"
+    ]]
+  });
 
     res.json(response);
   } catch (err) {
