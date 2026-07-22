afa57ea61b896df4a2fb8ddacd32fdd681bb7589
MilindSakpal <milind52sakpal@gmail.com>
2026-07-22 18:39:26 +0530

    Test4

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 
 const { getGraphClient } = require("../services/graphService");
 
-router.get("/drive", async (req, res) => {
+router.get("/files", async (req, res) => {
   try {
     const client = await getGraphClient();
 
-    const drive = await client
-      .api("/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive")
+    const files = await client
+      .api("/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/root/children")
       .get();
 
-    res.json(drive);
+    res.json(files);
 
   } catch (err) {
     console.error(err);
