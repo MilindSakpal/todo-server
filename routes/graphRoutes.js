1e656b7cae761e95e36065a486a32ea6ad869c2c
MilindSakpal <milind52sakpal@gmail.com>
2026-07-22 20:17:04 +0530

    Test4

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -19,22 +19,19 @@ router.get("/column-names", async (req, res) => {
   }
 });
 
-router.get("/columns", async (req, res) => {
+router.get("/rows", async (req, res) => {
   try {
     const client = await getGraphClient();
 
-    const columns = await client
+    const rows = await client
       .api(
-        "/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ3DXGXPOPIDDRHZTJA4BFHA2YFH/workbook/tables/TaskTable/columns"
+        "/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ3DXGXPOPIDDRHZTJA4BFHA2YFH/workbook/tables/TaskTable/rows"
       )
       .get();
 
-    res.json(columns);
+    res.json(rows);
   } catch (err) {
-    console.error(err);
-
     res.status(500).json({
-      success: false,
       message: err.message,
       body: err.body,
     });
