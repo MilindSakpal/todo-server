8a74ee3c964c0e70ff52eb2a9637ccac75cb5df3
MilindSakpal <milind52sakpal@gmail.com>
2026-07-22 18:41:49 +0530

    Test4

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const { getGraphClient } = require("../services/graphService");
 
-router.get("/files", async (req, res) => {
+router.get("/tracker-files", async (req, res) => {
   try {
     const client = await getGraphClient();
 
     const files = await client
-      .api("/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/root/children")
+      .api("/users/4e2610c5-da92-480b-80a8-8b05ff2724e5/drive/items/01RFGZXZ6TZVCOON5MFVCYJGYCSA6ORWB2/children")
       .get();
 
     res.json(files);
