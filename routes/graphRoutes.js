eea7620bf3a1b4a503cb952de6126fb851803fac
MilindSakpal <milind52sakpal@gmail.com>
2026-07-22 18:19:07 +0530

    Test1

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -1,24 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getGraphClient } =  require("../services/graphService");
-
-router.get("/user", async (req, res) => {
-  try {
-    const client = await getGraphClient();
-
-    const user = await client
-      .api("/users/info@intellysisdigital.com")
-      .get();
-
-    res.json(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
+router.get("/user", (req, res) => {
+  res.json({
+    success: true,
+    message: "Graph router is working!"
+  });
 });
 
 module.exports = router;
